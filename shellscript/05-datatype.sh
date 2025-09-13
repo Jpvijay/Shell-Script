e430a05e56d618dfaf805803e2eea6d90fac6f55
jayaprakash <prakashenugonda@gmail.com>
2025-09-13 14:29:01 +0530

    taking variables inputs

--- shellscript/05-datatype.sh
+++ shellscript/05-datatype.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "give 2 numbers to add"
-READ NUM1,NUM2
+read NUM1
+read NUM2
 echo "given numbers $NUM1 & $NUM2"
-SUM=(($NUM1+$NUM2))
+SUM=$(($NUM1+$NUM2))
 echo "sum of given numbers: $SUM"
