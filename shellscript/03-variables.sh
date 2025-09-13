7c27377a60c0f2f79fc9a185978f1ca3c24ac88a
jayaprakash <prakashenugonda@gmail.com>
2025-09-13 13:54:01 +0530

    trying executing with variablesvariables

---
+++ shellscript/03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=RAM
+PERSON2=SITA
+
+echo "$PERSON1::Namasthe Sita"
+echo "$PERSON2::Namasthe Ram"
+echo "$PERSON1::Coffee?!!"
+echo "$PERSON2::y not!!"
+
+// in this way we can avoid repeating (DRY)
